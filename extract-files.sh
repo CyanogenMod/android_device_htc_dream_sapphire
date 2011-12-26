CyanogenMod/android_device_htc_dream_sapphire

commit ab160e72a938910858c4e66851e5d765b76bc2f3

D/S: Gingerbread overhaul. This is a combination of 13 commits.
D/S: Add more product overrides
D/S: Add Gingerbread GPS
D/S: Add Kineto Compat
D/S: Enable V8 JavaScript Engine
D/S: Add AAC encoding profiles
D/S: Update init files for GBread
D/S: Add a few product packages
D/S: Complete cleanup of prebuilt stuff
D/S: Remove AndroidBoard.mk code (is now legacy)
D/S: Add VendorSetup
D/S: Fix build break
D/S: Update kernel to 2.6.37 (No support for 32A or MT3G 1.2(Fender))
D/S: Update extract-files.sh

extract-files.sh
@@ -37,12 +37,6 @@ adb pull /system/lib/liboemcamera.so ../../../vendor/htc/$DEVICE/proprietary/lib
 adb pull /system/lib/libOmxH264Dec.so ../../../vendor/htc/$DEVICE/proprietary/libOmxH264Dec.so
 adb pull /system/lib/libOmxMpeg4Dec.so ../../../vendor/htc/$DEVICE/proprietary/libOmxMpeg4Dec.so
 adb pull /system/lib/libOmxVidEnc.so ../../../vendor/htc/$DEVICE/proprietary/libOmxVidEnc.so
-adb pull /system/lib/libomx_wmadec_sharedlibrary.so ../../../vendor/htc/$DEVICE/proprietary/libomx_wmadec_sharedlibrary.so
-adb pull /system/lib/libomx_wmvdec_sharedlibrary.so ../../../vendor/htc/$DEVICE/proprietary/libomx_wmvdec_sharedlibrary.so
-adb pull /system/lib/libpvasfcommon.so ../../../vendor/htc/$DEVICE/proprietary/libpvasfcommon.so
-adb pull /system/lib/libpvasflocalpbreg.so ../../../vendor/htc/$DEVICE/proprietary/libpvasflocalpbreg.so
-adb pull /system/lib/libpvasflocalpb.so ../../../vendor/htc/$DEVICE/proprietary/libpvasflocalpb.so
-adb pull /system/etc/pvasflocal.cfg ../../../vendor/htc/$DEVICE/proprietary/pvasflocal.cfg
 
 (cat << EOF) | sed s/__DEVICE__/$DEVICE/g > ../../../vendor/htc/$DEVICE/$DEVICE-vendor-blobs.mk
 # Copyright (C) 2010 The Android Open Source Project
@@ -89,16 +83,5 @@ ifndef WITH_DS_HTCACOUSTIC_HACK
 PRODUCT_COPY_FILES += \\
     vendor/htc/__DEVICE__/proprietary/AudioPara4.csv:system/etc/AudioPara4.csv
 endif
-
-ifdef WITH_WINDOWS_MEDIA
-PRODUCT_COPY_FILES += \\
-    vendor/htc/__DEVICE__/proprietary/libomx_wmadec_sharedlibrary.so:system/lib/libomx_wmadec_sharedlibrary.so \\
-    vendor/htc/__DEVICE__/proprietary/libomx_wmvdec_sharedlibrary.so:system/lib/libomx_wmvdec_sharedlibrary.so \\
-    vendor/htc/__DEVICE__/proprietary/libpvasfcommon.so:system/lib/libpvasfcommon.so \\
-    vendor/htc/__DEVICE__/proprietary/libpvasflocalpbreg.so:system/lib/libpvasflocalpbreg.so \\
-    vendor/htc/__DEVICE__/proprietary/libpvasflocalpb.so:system/lib/libpvasflocalpb.so \\
-    vendor/htc/__DEVICE__/proprietary/pvasflocal.cfg:system/etc/pvasflocal.cfg
-endif
-EOF
 
 ./setup-makefiles.sh

AndroidProducts.mk
@@ -13,17 +13,9 @@
 # limitations under the License.
 
 #
-# AndroidBoard.mk is a legacy mechanism to deal with a few
-# edge-cases that can't be managed otherwise. No new rules
-# should be added to this file.
+# This file lists the product definition files that define
+# configurations which are actually buildable (e.g. through lunch)
 #
 
-LOCAL_PATH := $(call my-dir)
-
-# Least specific includes go first, so that they can get
-# overridden further down
-
-include $(CLEAR_VARS)
-
-# include the non-open-source counterpart to this file
--include vendor/htc/dream_sapphire/AndroidBoardVendor.mk
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/full_dream_sapphire.mk

BoardConfig.mk
@@ -22,6 +22,8 @@
 # against the traditional rules of inheritance).
 USE_CAMERA_STUB := true
 
+JS_ENGINE := v8
+
 # inherit from the proprietary version
 -include vendor/htc/dream_sapphire/BoardConfigVendor.mk
 
@@ -59,6 +61,12 @@ BOARD_USES_QCOM_LIBS := true
 
 BOARD_USE_FROYO_LIBCAMERA := true
 
+BOARD_USES_GPSSHIM:=true
+
+BOARD_GPS_LIBRARIES := libgps
+
+BOARD_USE_KINETO_COMPATIBILITY := true
+
 TARGET_RELEASETOOLS_EXTENSIONS := device/htc/common
 
 TARGET_BOOTLOADER_BOARD_NAME := sapphire
@@ -80,3 +88,4 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x127c0000
 
 #TARGET_RECOVERY_UI_LIB := librecovery_ui_sapphire librecovery_ui_htc
 TARGET_PREBUILT_KERNEL := device/htc/dream_sapphire/kernel
+LOCAL_KERNEL := device/htc/dream_sapphire/kernel

full_dream_sapphire.mk
@@ -33,8 +33,12 @@ PRODUCT_PACKAGES += \
 	vold.fstab \
 	wlan_loader \
 	tiwlan.ini \
-	libOmxCore
-	
+	libOmxCore \
+	gps.trout \
+	gps.sapphire \
+	copybit.msm7k \
+	gralloc.msm7k \
+	lights.msm7k
 
 # Install the features available on this device.
 PRODUCT_COPY_FILES += \
@@ -45,11 +49,43 @@ PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
     frameworks/base/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml
 
+# Prebuilt stuff
+PRODUCT_COPY_FILES += \
+    device/htc/dream_sapphire/prebuilt/init.trout.rc:root/init.trout.rc \
+    device/htc/dream_sapphire/prebuilt/init.sapphire.rc:root/init.sapphire.rc \
+    device/htc/dream_sapphire/prebuilt/build.trout.prop:system/build.trout.prop \
+    device/htc/dream_sapphire/prebuilt/build.sapphire.prop:system/build.sapphire.prop \
+    device/htc/dream_sapphire/prebuilt/ueventd.trout.rc:root/ueventd.trout.rc \
+    device/htc/dream_sapphire/prebuilt/ueventd.sapphire.rc:root/ueventd.sapphire.rc \
+    device/htc/dream_sapphire/prebuilt/vold.fstab:system/etc/vold.fstab
+
+# Keychars / Keylayout / Headset
+PRODUCT_COPY_FILES += \
+    device/htc/dream_sapphire/keylayout/trout-keypad.kcm:system/usr/keychars/trout-keypad.kcm \
+    device/htc/dream_sapphire/keylayout/trout-keypad-qwertz.kcm:system/usr/keychars/trout-keypad-qwertz.kcm \
+    device/htc/dream_sapphire/keylayout/trout-keypad-v2.kcm:system/usr/keychars/trout-keypad-v2.kcm \
+    device/htc/dream_sapphire/keylayout/trout-keypad-v3.kcm:system/usr/keychars/trout-keypad-v2.kcm \
+    device/htc/dream_sapphire/keylayout/trout-keypad.kl:system/usr/keylayout/trout-keypad.kl \
+    device/htc/dream_sapphire/keylayout/trout-keypad-qwertz.kl:system/usr/keylayout/trout-keypad-qwertz.kl \
+    device/htc/dream_sapphire/keylayout/trout-keypad-v2.kl:system/usr/keylayout/trout-keypad-v2.kl \
+    device/htc/dream_sapphire/keylayout/trout-keypad-v3.kl:system/usr/keylayout/trout-keypad-v2.kl \
+    device/htc/dream_sapphire/keylayout/sapphire-keypad.kcm:system/usr/keychars/trout-keypad.kcm \
+    device/htc/dream_sapphire/keylayout/sapphire-keypad.kl:system/usr/keylayout/trout-keypad.kl \
+    device/htc/dream_sapphire/keylayout/h2w_headset.kl:system/usr/keylayout/h2w_headset.kl
+
 PRODUCT_PROPERTY_OVERRIDES := \
     keyguard.no_require_sim=true \
     ro.ril.hsxpa=2 \
     ro.ril.gprsclass=10 \
-    ro.media.dec.jpeg.memcap=10000000
+    ro.media.dec.jpeg.memcap=10000000 \
+    ro.com.google.clientidbase=android-tmobile-us \
+    ro.com.google.clientidbase.vs=android-hms-tmobile-us \
+    ro.com.google.clientidbase.ms=android-hms-tmobile-us \
+    ro.com.google.locationfeatures=1 \
+    ro.com.google.networklocation=1 \
+    ro.setupwizard.enable_bypass=1 \
+    ro.media.dec.aud.wma.enabled=1 \
+    ro.media.dec.vid.wmv.enabled=1
 
 PRODUCT_PROPERTY_OVERRIDES += \
     rild.libpath=/system/lib/libhtc_ril.so \
@@ -84,6 +120,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
     device/htc/dream_sapphire/media_profiles.xml:/system/etc/media_profiles.xml
 
+# Kernel stuff
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 	LOCAL_KERNEL := device/htc/dream_sapphire/kernel
 else
@@ -91,7 +128,8 @@ else
 endif
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel
+    $(LOCAL_KERNEL):kernel \
+    device/htc/dream_sapphire/prebuilt/wlan.ko:/system/lib/modules/wlan.ko \
 
 ## (2) Also get non-open-source aspects if available
 $(call inherit-product-if-exists, vendor/htc/dream_sapphire/dream_sapphire-vendor.mk)

commit e7b04fe58eb399da3cecadafbcbc8e9f711a7958

Added "EOF" before "./setup-makefiles.sh" to enable compilation to succeed - see http://forum.cyanogenmod.com/topic/38764-cm72-bugfix-solution-offered/ for details. This error was introduced in commit ab160e72a938910858c4e66851e5d765b76bc2f3 (wesgarner - 20 January 2011).

extract-files.sh
@@ -83,5 +83,6 @@ ifndef WITH_DS_HTCACOUSTIC_HACK
 PRODUCT_COPY_FILES += \\
     vendor/htc/__DEVICE__/proprietary/AudioPara4.csv:system/etc/AudioPara4.csv
 endif
+EOF
 
 ./setup-makefiles.sh